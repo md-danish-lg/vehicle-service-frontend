0c156ea8770473826278f4b49d8ba7bad0c570e4
md-danish-lg <dk665226@gmail.com>
2026-07-12 14:55:00 +0500

    add Card component and update App to include it; format index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Button } from "./components/ui/Button";
+import { Card } from "./components/ui/Card";
 
 export default function App() {
-    return <h1 className="text-3xl font-bold underline">
-        Hello world!
-
+    return( <>
+        <h1 className="text-3xl font-bold underline"></h1>
+            Hello world!
         <Button variant="hollow" size="lg">
             Add Vehicle
         </Button>
-    </h1>
+        <Card>
+            <h1>This is a card component</h1>
+        </Card>
+        </>
+    )
 }
---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+
+
+type cardProps = {
+    children: ReactNode;
+} 
+
+export function Card({children}: cardProps) {
+    return (
+        <div className="bg-blue-200/5 border border-white/10 shadow-md rounded-sm p-6 text-white">
+            {children}
+        </div>
+    )
+
+
+}
